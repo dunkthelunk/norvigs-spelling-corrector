6caf3a966245e5e932737d60f41c2b64e705ad9c
dunkthelunk <prudhvi.porandla@icloud.com>
2018-07-30 16:03:54 +0530

    add image

--- Main.cpp
+++ Main.cpp
@@ -10,14 +10,14 @@ int main() {
   SpellingCorrector.loadWords(File);
 
   std::string InputWord;
-  std::cout << "Enter word to correct (Ctrl+D to exit) : ";
+  std::cout << "Enter word to correct (^D to exit) : ";
   while (std::cin >> InputWord) {
     auto start = std::chrono::system_clock::now();
     std::cout << SpellingCorrector.correct(InputWord) << '\n';
     auto end = std::chrono::system_clock::now();
     std::chrono::duration<double> elapsed_seconds = end - start;
     std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
-    std::cout << "Enter word to correct (Ctrl+D to exit) : ";
+    std::cout << "Enter word to correct (^D to exit) : ";
   }
   std::cout << '\n';
   return 0;
